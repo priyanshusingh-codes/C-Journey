47100c6eee8ef99aec94427a1a2112a468e5df31
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-15 16:45:08 +0530

    Adding_two_numbers

---
+++ Basics_OF_C/Adding_two_numbers.c
@@ -0,0 +1,13 @@
+//Addition of two numbers in C
+#include <stdio.h>
+int main ()
+{
+    int number  = 100;
+    float amount = 30.75 + 75.35;
+
+    printf("%d\n", number);
+    printf("%5.2f\n", amount);
+
+    return 0;
+
+}
