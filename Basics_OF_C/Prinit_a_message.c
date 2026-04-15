419f8b7f367a21dd58079088da8becec6b0b7cfd
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-15 16:19:46 +0530

    Print_a_message

---
+++ Basics_OF_C/Prinit_a_message.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Include the standard input/output library
+int main () // Main function - entry point of the program
+{
+   printf("Hello, World!"); // Print "Hello, World!" to the console
+   return 0; // Return 0 to indicate successful execution
+}
