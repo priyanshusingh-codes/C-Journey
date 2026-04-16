04b84d2578880948020a1cac160b2cb0e11f9675
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-16 17:39:55 +0530

    Practice1.c

---
+++ Basics_OF_C/Practice1.c
@@ -0,0 +1,15 @@
+/*
+Write a program that will print your mailing adress in following form :
+        First Line :  Name
+        Second Line:  Door No, Street
+        Third Line : City, Pin Code
+*/
+
+#include <stdio.h>
+int main()
+{
+    printf("Michael Anderson\n");
+    printf("742 Evergreen Terrace\n");
+    printf("Springfield, 62704\n");
+    return 0;
+}
