f2131b230bb15f36866db7624611547bc94acce4
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-17 01:51:24 +0530

    Swap.c

---
+++ Basics_OF_C/Swap.c
@@ -0,0 +1,18 @@
+//To swap two numbers
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &a, &b);
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    printf("After swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
