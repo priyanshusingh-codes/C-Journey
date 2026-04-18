b34b7df61affae2da5ab621edd6277e25f6622b2
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-18 21:52:17 +0530

    Introduction.c

---
+++ Constants_Variables_and_Data_Types/Introduction.c
@@ -0,0 +1,71 @@
+/*
+#Character set => The characters that can be used to form words, NUmbers and expressions.
+Note - Compiler ignores white spaces unless they are part is string constant
+
+Type of C character set :
+
+i)   Letters => UpperCase (A to Z) + Lowercase (a to z)
+ii)  Digits => 0 to 9
+iii) Special Characters
+     , Comma
+     . Period
+     ; Semicolon
+     : Colon
+     ? Question mark
+     ' Aostrophe
+     " Quotation Mark
+     ! Exclamation mark
+     | Vertical bar
+     / Slash
+     \ Backslash
+     ~ Tlide
+     _ Under score
+     $ Dollar Sign
+     % percent sign
+     & ampersand
+     ^ Caret
+     * Asterisk
+     - Minus Sign
+     + Plus SIgn
+     < Opening angle bracket
+     > Closing angle bracket
+     ( left paranthesis 
+     ) right paranthesis
+     [ left bracket
+     ] right bracket
+     { left brace
+     } right brace
+     # number sign
+
+
+(iv) White Space     
+
+    Blank space
+    Horizontal tab
+    Carriage return
+    New Line
+    Form feed
+
+*/
+
+/*
+Trigraph Sequence
+//Not used in modern compilers
+??=  ->  #
+??/  ->  \
+??'  ->  ^
+
+*/
+
+/*
+C Tokens => Individual words and punchuation  marks are called Tokens
+Note : Tokens are the smallest individual units of a C program.
+
+i)  Keywords
+ii) Identifiers
+iii)Constants
+iv) Strings
+v)  Operators
+vi) Punctuators (Special Symbbols)
+
+*/
