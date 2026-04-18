6f0e901c0874e7c6144c64b4f0c0000efce0d10b
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-18 23:18:16 +0530

    Data_Types.c

---
+++ Constants_Variables_and_Data_Types/Data_Types.c
@@ -0,0 +1,55 @@
+/*
+Data types tell the compiler what kind of data a variable can store.
+
+Types of Data Types in C
+1. Basic (Primary) Data Types
+
+These are the most commonly used:
+int      // integers (10, -5)
+float    // decimal numbers (3.14)
+char     // single character ('A')
+double   // large decimal numbers
+
+2. Derived Data Types
+
+Built from basic types:
+array
+pointer
+function
+
+Example:
+int arr[5];   // array
+int *ptr;     // pointer
+
+3. User-Defined Data Types
+
+Created by the programmer:
+struct
+union
+enum
+typedef
+
+
+4. Void Data Type
+
+void
+//Means no value / no type
+Used in functions:
+
+void display() {
+    // no return value
+}
+
+*/
+
+
+
+/*
+Memory Size //Important
+
+Data Type	Typical Size :
+int	4 bytes
+float	4 bytes
+char	1 byte
+double	8 bytes
+*/
