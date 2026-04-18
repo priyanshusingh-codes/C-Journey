caf885ab32da11c82e6a520c13d2563c05679bb6
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-18 22:03:28 +0530

    Keywords_and_Identifiers.c

---
+++ Constants_Variables_and_Data_Types/Keywords_and_Identifiers.c
@@ -0,0 +1,32 @@
+/*
+Keywords => Serve as basic building blocks of program statement.
+Note : All keywords must be written in lower case.
+Note : You cannot use them as variable names.
+Note : Keywords are predefined by the C language
+
+C Keywords :
+
+auto, break, case, char, const, continue,
+default, do, double, else, enum, extern,
+float, for, goto, if, int, long,
+register, return, short, signed, sizeof,
+static, struct, switch, typedef, union,
+unsigned, void, volatile, while
+*/
+
+/*
+Identifiers : Identifiers are the names you give to variables, functions, arrays, etc.
+Note : They are user-defined names used to identify something in your program.
+
+Rules:
+i)  Must start with a letter (A–Z, a–z) or underscore _
+ii) Can contain letters, digits, underscore
+iii)Cannot be a keyword
+iv) No spaces or special symbols (@, #, %, etc.)
+
+Example :
+int age;
+int _count;
+int total_marks;
+int value123;
+*/
