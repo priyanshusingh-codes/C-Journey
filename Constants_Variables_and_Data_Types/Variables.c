b8623f24a5b71ab35609c1f69ecc38074b36e1fc
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-18 22:33:46 +0530

    Variables.c

---
+++ Constants_Variables_and_Data_Types/Variables.c
@@ -0,0 +1,12 @@
+/*
+Variable : A variable is a named memory location used to store data.
+
+//In simple words:
+It’s a container that holds a value which can change during program execution.
+
+Example :
+int age = 20;
+int → data type
+age → variable (identifier)
+20 → value stored
+*/
