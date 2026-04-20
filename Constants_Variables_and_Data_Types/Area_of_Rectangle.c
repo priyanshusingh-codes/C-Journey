2f582a611cf5f6a5f17f7b021d7913f0e68a1bdb
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-20 23:02:16 +0530

    Area_of_Rectangle.c

---
+++ Constants_Variables_and_Data_Types/Area_of_Rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define LENGTH 10   // length of rectangle
+#define WIDTH 5     // width of rectangle
+
+int main() {
+    int area;
+
+    area = LENGTH * WIDTH;   // calculation using constants
+
+    printf("Area of rectangle = %d\n", area);
+
+    return 0;
+}
