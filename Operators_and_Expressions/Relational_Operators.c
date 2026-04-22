50dac7e52607e122b9cecfa4286ec1910310b023
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-22 23:23:27 +0530

    Relational_Operators.c

---
+++ Operators_and_Expressions/Relational_Operators.c
@@ -0,0 +1,21 @@
+/*
+Relational Operators
+
+These operators are used to compare two values.
+They always return:
+1 (true)
+0 (false)
+
+| Operator | Meaning                  | Example  | Result               |
+| -------- | ------------------------ | -------- | -------------------- |
+| `==`     | Equal to                 | `a == b` | True if equal        |
+| `!=`     | Not equal to             | `a != b` | True if not equal    |
+| `>`      | Greater than             | `a > b`  | True if a is greater |
+| `<`      | Less than                | `a < b`  | True if a is smaller |
+| `>=`     | Greater than or equal to | `a >= b` | True if a ≥ b        |
+| `<=`     | Less than or equal to    | `a <= b` | True if a ≤ b        |
+
+
+= (assignment) ❌
+== (comparison) ✅
+*/
