a932af927cd844be1d1635e9b4a3906a18cc2f69
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-22 23:09:53 +0530

    Introduction.c

---
+++ Operators_and_Expressions/Introduction.c
@@ -0,0 +1,16 @@
+//An Operator is an symbol that tells the computer to perform a certain mathematical or logical manipulations.
+
+/*
+Types of Operators -
+
+i)  Arithmetic Operators
+ii) Relational Operators
+iii)Logical Operators
+iv) Assignment Operators
+v)  Increment Operators and decrement Operators
+vi) Conditional Operators
+vii)Bitwise Operators
+viii)Special Operators
+
+
+*/
