6393617d0282bd59f750ddff76ec63fc76d6a471
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-22 23:14:34 +0530

    Arithmetic_Operators.c

---
+++ Operators_and_Expressions/Arithmetic_Operators.c
@@ -0,0 +1,18 @@
+/*
+
+Operator   |  Name          | Example | Meaning                  |
+| -------- | -------------- | ------- | ------------------------ |
+| `+`      | Addition       | `a + b` | Adds two numbers         |
+| `-`      | Subtraction    | `a - b` | Subtracts b from a       |
+| `*`      | Multiplication | `a * b` | Multiplies two numbers   |
+| `/`      | Division       | `a / b` | Divides a by b           |
+| `%`      | Modulus        | `a % b` | Remainder after division |
+
+| Operator | Name      | Example | Meaning              |
+| -------- | --------- | ------- | -------------------- |
+| `++`     | Increment | `a++`   | Increases value by 1 |
+| `--`     | Decrement | `a--`   | Decreases value by 1 |
+
+//Here a and b are called operands
+
+*/
