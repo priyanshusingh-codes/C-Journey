591305630aea79e520d6c3e2c1c4672293018cf9
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-23 21:27:17 +0530

    Assignment_Operator.c

---
+++ Operators_and_Expressions/Assignment_Operator.c
@@ -0,0 +1,32 @@
+//Assignment Operators are used to assign the result of a expresssion into a variable.
+/*
+//C has a set of 'shorthand' assignment operator of the form
+            v op = exp;
+            v is variable
+            exp is expression
+            op is operation
+example:
+
+x = x + (y+1) => x += y + 1;
+
+horthand Assignment Operators :
+
+a = a + 1      => a += 1
+a = a - 1      => a -= 1
+a = a* (n+1)   => a *= n+1
+a = a/ (n+1)   => a /= n+1
+a = a % b      => a %= b
+
+Addvantages
+i) What appears on the left-hand side need not to be repeated and therefore easier top write.
+ii)The statement is more efficient .
+iii)The statement is more concise and easier to read.
+
+Example:
+
+value(5*j-2) = value(5*j-2) + delta => vaue(5*j-2) += delta
+
+
+
+            
+*/
