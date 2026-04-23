589b17da6b517aceee4c40b499a941df364fe602
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-23 22:04:40 +0530

    Conditional_Operator.c

---
+++ Operators_and_Expressions/Conditional_Operator.c
@@ -0,0 +1,22 @@
+/*
+It is also called the ternary operator (because it uses 3 parts).
+The operator pair is "?,:"
+
+Syntax:
+
+exp1 ? exp2 : exp3
+
+example :
+
+a = 10;
+b = 15;
+x = (a > b) ? a : b;
+
+//It can be also used using if - else statement
+
+if (a > b)
+x = a;
+
+else 
+x = b;
+*/
