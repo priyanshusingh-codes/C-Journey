3ba38670b65895e3325e76b36eec93ff45030df6
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-24 21:40:20 +0530

    Math_Function.c

---
+++ Operators_and_Expressions/Math_Function.c
@@ -0,0 +1,39 @@
+/*
+Mathematical functions are built-in functions used to perform mathematical calculations like square root, power, logarithm, etc.
+
+These functions are available in the header file: <math.h>
+
++----------------------+----------------------+----------------------------------+
+| CATEGORY             | FUNCTION             | DESCRIPTION                      |
++----------------------+----------------------+----------------------------------+
+| TRIGONOMETRIC        | sin(x)               | Sine of angle (radians)          |
+|                      | cos(x)               | Cosine of angle                  |
+|                      | tan(x)               | Tangent of angle                 |
+|                      | asin(x)              | Inverse sine                     |
+|                      | acos(x)              | Inverse cosine                   |
+|                      | atan(x)              | Inverse tangent                  |
++----------------------+----------------------+----------------------------------+
+| HYPERBOLIC           | sinh(x)              | Hyperbolic sine                  |
+|                      | cosh(x)              | Hyperbolic cosine                |
+|                      | tanh(x)              | Hyperbolic tangent               |
+|                      | asinh(x)             | Inverse hyperbolic sine          |
+|                      | acosh(x)             | Inverse hyperbolic cosine        |
+|                      | atanh(x)             | Inverse hyperbolic tangent       |
++----------------------+----------------------+----------------------------------+
+| EXPONENTIAL & LOG    | exp(x)               | e^x                              |
+|                      | log(x)               | Natural log (base e)             |
+|                      | log10(x)             | Log base 10                      |
++----------------------+----------------------+----------------------------------+
+| POWER & ROOT         | pow(x, y)            | x raised to power y              |
+|                      | sqrt(x)              | Square root                      |
++----------------------+----------------------+----------------------------------+
+| ROUNDING             | ceil(x)              | Smallest integer ≥ x             |
+|                      | floor(x)             | Greatest integer ≤ x             |
+|                      | round(x)             | Nearest integer                  |
++----------------------+----------------------+----------------------------------+
+| ABSOLUTE & OTHER     | fabs(x)              | Absolute value (float/double)    |
+|                      | abs(x)               | Absolute value (integer)         |
+|                      | fmod(x, y)           | Remainder of division            |
+|                      | hypot(x, y)          | sqrt(x^2 + y^2)                  |
++----------------------+----------------------+----------------------------------+
+*/
