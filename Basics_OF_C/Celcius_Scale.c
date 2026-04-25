79f661f2c88b277d68ee0210eebd41f588725fb1
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-04-25 22:49:57 +0530

    Celcius_Scale.c

--- Basics_OF_C/Celcius_Scale.c
+++ Basics_OF_C/Celcius_Scale.c
@@ -1,13 +1,14 @@
 // To change farenheit scale to celcius scale
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     float fahrenheit, celsius;
 
     printf("Enter temperature in Fahrenheit: ");
     scanf("%f", &fahrenheit);
 
-    celsius = (5.0 / 9.0) * (fahrenheit - 32);
+    celsius = (5.0 / 9.0) * (fahrenheit - 32);//Formula to convert farenheit scale to celsius scale
 
     printf("Temperature in Celsius = %.2f\n", celsius);
 
