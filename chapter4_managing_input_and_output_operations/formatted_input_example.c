41b93935b92a13ecdc2c63ef57e3ccd9920b26d8
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-04-30 23:51:45 +0530

    chapter4_managing_input_and_output_operations/

---
+++ chapter4_managing_input_and_output_operations/formatted_input_example.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c, x, y, z;
+    int p, q, r;
+    printf("Enter three integers\n");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d %d %d,\n\n", a, b, c);
+    printf("Enter two 4-digit number\n");
+    scanf("%2d %4d", &x, &y);
+    printf("%d %d\n\n", x,y);
+    printf("Enter two integers\n");
+    scanf("%d %d", &a, &x);
+    printf("Enter a nine digit number\n");
+    scanf("%3d %4d %3d", &p, &q, &r);
+    printf("%d %d %d, \n\n", p, q, r);
+    printf("Enter two three digit numbers\n");
+    scanf("%d %d", &x, &y);
+    printf("%d %d", x, y);
+return 0;
+}
