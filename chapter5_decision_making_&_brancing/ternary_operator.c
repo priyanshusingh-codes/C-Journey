f181b1bbad2baa8ab0c31b204554f92a3577df54
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-03 23:16:56 +0530

    ternary_operator.c

---
+++ chapter5_decision_making_&_brancing/ternary_operator.c
@@ -0,0 +1,20 @@
+/*
+
+
+C has a special operator for two-way decisions.
+
+It is called:
+Conditional Operator
+Ternary Operator (because it uses 3 operands)
+
+Syntax
+condition ? expression1 : expression2;
+Meaning
+
+If condition is true:
+expression1 runs
+
+If false:
+expression2 runs
+
+*/
