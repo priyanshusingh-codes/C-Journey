ea11a24fd2a42ae2e97367416c55ef6faf4c084f
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-03 22:49:56 +0530

    if_else.c

---
+++ chapter5_decision_making_&_brancing/if_else.c
@@ -0,0 +1,21 @@
+/*
+
+
+if...else Statement
+Used when one action happens if true, another if fals
+
+Syntax:
+if (condition)
+{
+   true_block;
+}
+else
+{
+   false_block;
+}
+
+Only one block runs.
+
+*/
+
+
