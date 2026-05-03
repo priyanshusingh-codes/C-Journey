e328cf9e1fc2a1deccf04e84b27f7a029f5c3b26
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-03 22:39:33 +0530

    introduction.c

---
+++ chapter5_decision_making_&_brancing/introduction.c
@@ -0,0 +1,36 @@
+/*
+
+Introduction
+
+Normally, C programs run sequentially:
+
+statement1;
+statement2;
+statement3;
+
+But real programs need:
+decisions
+choices
+conditions
+different paths
+
+Example:
+If marks > 90 → Grade A
+If room dark → switch on light
+If balance = 0 → borrow money
+
+So C provides control statements.
+
+
+Decision Making Statements in C
+if
+switch
+Conditional operator ?:
+goto
+
+These are called:
+
+Decision-making statements
+Control statements
+
+*/
