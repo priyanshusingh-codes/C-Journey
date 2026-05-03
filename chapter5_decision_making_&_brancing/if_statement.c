3a37166f69766e61cef7615c4ffe305941978fd4
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-03 22:37:59 +0530

    if_statement.c

---
+++ chapter5_decision_making_&_brancing/if_statement.c
@@ -0,0 +1,26 @@
+/*
+
+if Statement : Used for two-way decision making.
+
+Syntax:
+if (condition)
+    statement;
+
+If condition is true : statement executes
+
+If false : skipped
+
+| Value    | Meaning |
+| -------- | ------- |
+| `0`      | False   |
+| Non-zero | True    |
+
+
+
+Forms of if :
+
+1. Simple if
+2. if...else
+3. Nested if...else
+4. else if ladder
+*/
