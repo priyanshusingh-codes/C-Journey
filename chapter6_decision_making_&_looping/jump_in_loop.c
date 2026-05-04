9fae4c1b4edd30026aea8284695702bc70b5cb80
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-04 23:21:56 +0530

    jump_in_loop.c

---
+++ chapter6_decision_making_&_looping/jump_in_loop.c
@@ -0,0 +1,121 @@
+/*
+
+Why “Jumps in Loops” exist?
+Normally, loops run until condition becomes false.
+
+But sometimes we want:
+
+Exit early 
+Skip some part 
+Jump to another place 
+
+*/
+
+// 1. break => Exit the loop immediately
+//Example: Stop when number found
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+        if (i == 5) {
+            break;
+        }
+        printf("%d ", i);
+    }
+
+    return 0;
+}
+
+/*
+Output:
+1 2 3 4
+Loop stops as soon as i == 5
+
+
+break exits only ONE loop
+ */
+
+
+
+
+
+ 
+ 
+//Continue → Skip current iteration
+//Example: Skip even numbers
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {
+            continue;
+        }
+        printf("%d ", i);
+    }
+
+    return 0;
+}
+
+/*
+Output:
+1 3 5 7 9
+Skips even numbers
+Loop continues normally
+ 
+
+ */
+
+
+ //3. goto → Jump anywhere (⚠️ dangerous)
+//Example:
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+start:
+    if (i > 5)
+        return 0;
+
+    printf("%d ", i);
+    i++;
+
+    goto start;
+}
+
+/*
+Output:
+1 2 3 4 5
+
+
+Why avoid goto?
+Makes code messy 
+Hard to debug
+Breaks structure
+
+Use only in rare cases (deep nested loops, error handling)
+
+*/
+
+
+
+
+//4. exit() → Exit entire program
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    if (n < 0) {
+        exit(0);
+    }
+
+    printf("Valid number");
+}
+
+//Program stops completely
