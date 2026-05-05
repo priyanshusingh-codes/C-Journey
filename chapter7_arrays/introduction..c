847d18d0bbf2313f83f64cf0514f67f8db8474aa
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-05 22:57:12 +0530

    introduction..c

---
+++ chapter7_arrays/introduction..c
@@ -0,0 +1,80 @@
+/*
+
+ARRAYS IN C
+-------------
+
+
+1. Introduction
+Basic data types in C: int, char, float, double
+Limitation: Each variable stores only one value
+Real-world programs require handling large amounts of data
+-----------------
+Solution: Arrays
+
+
+
+
+2. Definition of Array
+An array is a fixed-size, ordered collection of elements of the same data type
+It is a way to store multiple values using a single name
+
+
+
+
+
+3. Key Features of Arrays
+Stores homogeneous data (same type)
+Elements are stored in continuous memory locations
+Each element is accessed using an index (subscript)
+Index starts from 0
+
+
+4. Example : int salary[10];
+salary → array name
+10 → number of elements
+salary[0] → 1st employee salary
+salary[9] → 10th employee salary
+
+
+
+5. Terminology
+Array → complete collection
+Element → individual value inside array
+Index/Subscript → position of element
+
+
+
+6. Applications of Arrays
+Arrays are used to store:
+Temperature records (daily/monthly/yearly)
+List of employees
+Product prices in a store
+Student test scores
+Customer details (like phone numbers)
+Rainfall data
+
+
+
+7. Advantages
+Efficient storage of large data
+Easy access using index
+Helps write concise programs
+Works well with loops for:
+Input
+Processing
+Output
+
+
+8. Arrays as Data Structures
+Arrays are a type of data structure
+They organize and manage data efficiently
+
+Other data structures in C:
+
+Structures
+Linked Lists
+Stacks
+Queues
+Trees
+Graphs
+*/
