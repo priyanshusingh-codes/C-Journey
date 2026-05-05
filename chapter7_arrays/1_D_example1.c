6e7a1ac3d5c293a4858765d67e5ba204ba504ce5
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-05 23:42:11 +0530

    1_D_example1.c

---
+++ chapter7_arrays/1_D_example1.c
@@ -0,0 +1,20 @@
+//Sum of Elements
+
+#include <stdio.h>
+
+int main() {
+    int arr[5], sum = 0;
+
+    printf("Enter 5 numbers:\n");
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
