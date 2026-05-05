4215bded73b0f78185645417a52820cd20b7e40f
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-05 23:42:56 +0530

    1_D_example2.c

---
+++ chapter7_arrays/1_D_example2.c
@@ -0,0 +1,24 @@
+//Largest Element
+
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+
+    printf("Enter 5 numbers:\n");
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+
+    for(int i = 1; i < 5; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest = %d", max);
+
+    return 0;
+}
